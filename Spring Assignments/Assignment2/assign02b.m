95c4b35d757be8c8d0777452c6b46cdf1f258e08
Mousumi44 <54248800+Mousumi44@users.noreply.github.com>
2020-01-29 16:46:29 -0600

    Update assign02b.m

--- Spring Assignments/Assignment2/assign02b.m
+++ Spring Assignments/Assignment2/assign02b.m
@@ -7,7 +7,7 @@ format loose
 PRINCIPAL = 4000; %dollars
 
 %****INPUT****
-rates = [4.00, 4.33, 4.67, 5.00]; %interest per period
+rates = linspace(4,5,4);; %interest per period
 time = 4;    %years
 
 %****COMPUTE****
