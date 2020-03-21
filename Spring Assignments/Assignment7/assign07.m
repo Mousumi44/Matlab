fabeda6fc86776c7211ac1f15654f6dc7d1dc1f6
Mousumi Akter <mousumiakter@Mousumis-MacBook-Air.local>
2020-03-20 20:51:05 -0500

    matlab file

---
+++ Spring Assignments/Assignment7/assign07.m
@@ -0,0 +1,38 @@
+﻿clc, clear all
+
+% ***** CONSTANT *****
+FILENAME = 'dragCoef07.txt';
+DENSITY = 1.225; % kg/m3,density of air at sea level
+FORCE = 350; % Newtons (N), force of the drag
+VELOCITY = 90/3.6; % or 25 m/s, velocity of object relative to the fluid
+% autonomous function to compute frontal area
+getArea = @(coef) (2*FORCE) ./ ( coef * DENSITY * VELOCITY ^ 2 );
+% ***** INPUT *****
+% open data file
+[ fileID, msg ] = fopen( FILENAME, 'r' );
+% does file exist?
+if fileID <= 0
+    fprintf( 'File not available\n' )
+else
+    % file exist continue
+    % read year and drag coefficient from file
+    car = 0;
+    while ~feof( fileID )
+    car = car + 1;
+    yearDrag( car, 1 ) = fscanf( fileID, '%f', 1 ); % year
+    yearDrag( car, 2 ) = fscanf( fileID, '%f', 1 ); % drag coefficient
+    end
+    % ***** COMPUTE *****
+    % compute the frontal area of all cars
+    allArea = getArea( yearDrag(:,2) );
+    % add frontal area as third column of matrix
+    yearDrag(:,3) = allArea';
+    % sort matrix by year column ascending order
+    sortMatrix = sortrows( yearDrag, 1 );
+    % get average frontal area
+    aveArea = mean( sortMatrix(:,3) );
+    % ***** OUTPUT *****
+    % print table again using fprintf()
+    printReport( sortMatrix, aveArea )
+    fclose( fileID );
+end
---
+++ Spring Assignments/Assignment7/printReport.m
@@ -0,0 +1,31 @@
+﻿% printReport.m used by assign07.m
+function [] = printReport( carMatrix, aveArea )
+% % print report and draw graph
+% print rtable
+fprintf( ' All Cars\n' )
+fprintf( 'Year Drag Frontal\n' )
+fprintf( ' Coef Area(m^2)\n' )
+for car = 1:length(carMatrix(:,1) )
+    fprintf( '%4.0f %4.2f %6.4f', carMatrix(car,:) )
+    if carMatrix(car,3) > aveArea
+        fprintf( ' *\n' )
+    else
+        fprintf( '\n' )
+    end
+end
+fprintf( '* Frontal area above area \n' )
+% draw a plot of the years and drag coef
+plotData( carMatrix( :,1 ), carMatrix( :,2 ) )
+end
+
+function [] = plotData( years, dragCoef )
+% draw a plot of the years and drag coef
+numCars = 1:length( years );
+[hAx,hLine1,hLine2] = plotyy( numCars, years, numCars, dragCoef );
+title( 'Years .vs Drag Coef' )
+xlabel( 'Car number' )
+hLine1.LineStyle = '--';
+hLine2.LineStyle = ':';
+ylabel(hAx(1),'Year') % left y-axis
+ylabel(hAx(2),'Drag Coef') % right y-axis
+end
